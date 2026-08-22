14b7fb1c76d3612461f0ae1238616853e5799d1a
gulshan29-kumar <gulshankumar29082006@gmail.com>
2026-08-22 13:30:12 +0530

    Add Same Difference [A] from Codeforces

---
+++ 2166/A - Same Difference/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+ 
+public class qwe {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int t=in.nextInt();
+        while(t-->0){
+            int a=in.nextInt();
+            String str=in.next();
+            char ch=str.charAt(a-1);
+            int count=0;
+            for(int i=0;i<a;i++){
+                if(str.charAt(i)==ch) count++;
+            }
+            System.out.println(a-count);
+        }
+    }
+}
