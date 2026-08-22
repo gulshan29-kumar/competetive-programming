67c023a67460cc190d00482e28d1e81abdf37d00
gulshan29-kumar <gulshankumar29082006@gmail.com>
2026-08-22 14:10:58 +0530

    Add The 67th Integer Problem [A] from Codeforces

---
+++ 2218/A - The 67th Integer Problem/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner(System.in);
+		int t=in.nextInt();
+		while(t-->0){
+		    int a=in.nextInt();
+		    System.out.println(a);
+		}
+ 
+	}
+}
