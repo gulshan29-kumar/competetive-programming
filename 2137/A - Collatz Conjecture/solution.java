598a2c64f39cb29d19af83ce780742e38cb917cd
gulshan29-kumar <gulshankumar29082006@gmail.com>
2026-08-22 13:19:58 +0530

    Add Collatz Conjecture [A] from Codeforces

---
+++ 2137/A - Collatz Conjecture/solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+ 
+public class qwe {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int t=in.nextInt();
+        while(t-->0){
+            int a=in.nextInt();
+            int b=in.nextInt();
+            while(a-->0){
+                b*=2;
+            }
+            System.out.println(b);
+        }
+    }
+}
