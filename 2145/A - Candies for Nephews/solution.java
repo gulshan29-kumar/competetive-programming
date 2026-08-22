a0f4466aff41d916db3bd489c0acd5e4dcfa8253
gulshan29-kumar <gulshankumar29082006@gmail.com>
2026-08-22 13:24:00 +0530

    Add Candies for Nephews [A] from Codeforces

---
+++ 2145/A - Candies for Nephews/solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ 
+public class qwe {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int a=in.nextInt();
+            if(a%3==0) System.out.println(0);
+            else System.out.println(3-a%3);
+        }
+    }
+}
