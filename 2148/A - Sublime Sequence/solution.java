873089b1a17b5d8167093ea7bc3400ea5b968a54
gulshan29-kumar <gulshankumar29082006@gmail.com>
2026-08-22 13:21:13 +0530

    Add Sublime Sequence [A] from Codeforces

---
+++ 2148/A - Sublime Sequence/solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class qwe {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int t=in.nextInt();
+        while(t-->0){
+            int a=in.nextInt();
+            int b=in.nextInt();
+            if(b%2==0) System.out.println(0);
+            else System.out.println(a);
+        }
+    }
+ 
+}
