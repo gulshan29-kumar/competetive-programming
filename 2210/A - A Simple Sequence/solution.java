844337c843f44fc6a10c00aa73ad2e5df6197a83
gulshan29-kumar <gulshankumar29082006@gmail.com>
2026-08-22 14:09:44 +0530

    Add A Simple Sequence [A] from Codeforces

---
+++ 2210/A - A Simple Sequence/solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner(System.in);
+		int t=in.nextInt();
+		while(t-->0){
+		    int a=in.nextInt();
+		    for(int i=a;i>=1;i--) System.out.print(i+" ");
+		    System.out.println();
+		}
+ 
+	}
+}
