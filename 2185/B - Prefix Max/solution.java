8c3193e7f88381d4a8370b5fbe8c21971935f201
gulshan29-kumar <gulshankumar29082006@gmail.com>
2026-08-22 14:12:19 +0530

    Add Prefix Max [B] from Codeforces

---
+++ 2185/B - Prefix Max/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner(System.in);
+		int t=in.nextInt();
+		while(t-->0){
+		    int a=in.nextInt();
+		    int max=1;
+		    for(int i=0;i<a;i++) max=Math.max(max,in.nextInt());
+		    System.out.println(max*a);
+		}
+ 
+	}
+}
