dec56c9e7f4d0244c077a10b8da723d6f54ed292
gulshan29-kumar <gulshankumar29082006@gmail.com>
2026-08-22 14:10:42 +0530

    Add Odd One Out [A] from Codeforces

---
+++ 2214/A - Odd One Out/solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		System.out.println("C"+2);
+ 
+	}
+}
