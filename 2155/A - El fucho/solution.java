b9f20a135d675d9c3910d77d9a132c4b4b8a8b79
gulshan29-kumar <gulshankumar29082006@gmail.com>
2026-08-22 13:23:52 +0530

    Add El fucho [A] from Codeforces

---
+++ 2155/A - El fucho/solution.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+ 
+public class qwe {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+         int a=in.nextInt();
+            System.out.println((2*a-2));
+        }
+    }
+}
