1cef7d440f0fee4832607edcefb36a466c8f1db2
gulshan29-kumar <gulshankumar29082006@gmail.com>
2026-09-03 22:43:43 +0530

    Add Koshary [A] from Codeforces

---
+++ 2227/A - Koshary/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b; 
+	    if(a%2==1&&b%2==1) cout<<"NO"<<endl;
+	    else cout<<"Yes"<<endl;
+	}
+ 
+}
